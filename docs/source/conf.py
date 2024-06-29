9dba36d1633cde94ab9f21d46317097389d84eee
Yoann Piétri <Yoann.Pietri@lip6.fr>
2024-06-29 10:48:40 +0200

    docs: :memo: Fix intersphinx.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -101,12 +101,12 @@ autodoc_member_order = "bysource"
 
 
 intersphinx_mapping = {
-    "qosst": ("https://qosst.readthedocs.io/", None),
-    "qosst-core": ("https://qosst-core.readthedocs.io/", None),
-    "qosst-hal": ("https://qosst-hal.readthedocs.io/", None),
-    "qosst-bob": ("https://qosst-bob.readthedocs.io/", None),
-    "qosst-skr": ("https://qosst-skr.readthedocs.io/", None),
-    "qosst-sim": ("https://qosst-sim.readthedocs.io/", None),
+    "qosst": ("https://qosst.readthedocs.io/en/latest/", None),
+    "qosst-core": ("https://qosst-core.readthedocs.io/en/latest/", None),
+    "qosst-hal": ("https://qosst-hal.readthedocs.io/en/latest/", None),
+    "qosst-bob": ("https://qosst-bob.readthedocs.io/en/latest/", None),
+    "qosst-skr": ("https://qosst-skr.readthedocs.io/en/latest/", None),
+    "qosst-sim": ("https://qosst-sim.readthedocs.io/en/latest/", None),
 }
 html_logo = "_static/qosst_logo_square_white.png"
 html_theme_options = {
